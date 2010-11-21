0b13bfb558c0728e45f45d924d64a5da42f7c62d
David Ackerman <dackerman@google.com>
2010-11-21 13:40:37 -0800

    added better animation and some images

--- js.js
+++ js.js
@@ -1,8 +1,14 @@
 animTimer = null;
 currentImg = null;
 
+updateSpeed = 50;
+
 r = function(x){
   return Math.floor(Math.random() * (x+1));
+};
+
+rr = function(l, h){
+  return Math.floor(Math.random() * (h-l) + l);
 };
 
 startFlying = function(selector) { 
@@ -11,10 +17,28 @@ startFlying = function(selector) {
     $(selector).each(function(index) {
       $(this).css('top', r(500));
       $(this).css('left', r(500));
+      $(this).data("particle", {vx:20, vy:20, ax:3, ay:3})
     });
     currentImg = selector;
-    animTimer = setInterval("updateImgs()", 50);
+    animTimer = setInterval("updateImgs()", updateSpeed);
   };
+};
+
+updateImgs = function() {
+  $(currentImg).each(function(index) {
+    var curPosition = $(this).position();
+    var particle = $(this).data("particle");
+    var seconds = updateSpeed / 1000.0;
+    particle.vx += particle.ax * seconds;
+    particle.vy += particle.ay * seconds;
+    curPosition.left += particle.vx * seconds;
+    curPosition.top += particle.vy * seconds;
+    particle.ax = particle.ax * 0.9 + rr(-5, 10);
+    particle.ay = particle.ay * 0.9 + rr(-5, 10);
+    $(this).data("particle", particle);
+    $(this).css('top', curPosition.top);
+    $(this).css('left', curPosition.left);
+  });
 };
 
 stopFlying = function(selector) {
@@ -22,14 +46,6 @@ stopFlying = function(selector) {
     clearInterval(animTimer);
     $(selector).addClass('hidden');
   };
-};
-
-updateImgs = function() {
-  $(currentImg).each(function(index) {
-    var curPosition = $(this).position();
-    $(this).css('top', curPosition.top + r(10)-5);
-    $(this).css('left', curPosition.left + r(10)-5);
-  });
 };
 
 $(document).ready(function() {
